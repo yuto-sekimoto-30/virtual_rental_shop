f1e927d5ce59be183f77bb626d90188f4f35ed73
yuto <maunaosakusaku@gmail.com>
2022-12-20 04:29:09 +0900

    映画一覧機能と詳細機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'movies/search'
-  get 'movies/show'
+  post 'movies/search', to: 'movies#search' 
+  resources :movies, only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'login', to: 'user_sessions#new'  # 追記
