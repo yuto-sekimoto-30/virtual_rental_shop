10db4fef96e3f5c1d196ffc06c39e1a0e6a337ba
yuto <maunaosakusaku@gmail.com>
2023-01-28 20:29:58 +0900

    検索機能form種類追加、コード一部修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,52 +22,43 @@ class MoviesController < ApplicationController
     end
 
     #人気映画取得
-    @movies_pops = JSON.parse(Tmdb::Movie.popular.to_json)['table']['results']
+    @popular_movies = JSON.parse(Tmdb::Movie.popular.to_json)['table']['results']
 
-    #検索映画取得
-    #検索名パラメータがある場合
-    if !params[:name].blank?
-      total_page = JSON.parse((Tmdb::Search.movie(params[:name])).to_json)['table']['total_pages']
-      name = true
-    elsif !params[:genre].blank?
-      total_page = JSON.parse((Tmdb::Genre.movies(params[:genre])).to_json)['table']['total_pages']
-      genre = true
-    else
-      rand_alpha = ('a'..'z').to_a.shuffle[0]
-      total_page = JSON.parse((Tmdb::Search.movie(rand_alpha)).to_json)['table']['total_pages']
-      random = true
-    end
-
-    i = 1
+    #映画情報取得
+    search = false
     @search_movies = []
-    total_page.times do 
-      if name || random
-        param_name = params[:name] if name
-        param_name = rand_alpha if random
-        @search_movies += JSON.parse((Tmdb::Search.movie(param_name, page: "#{i}")).to_json)['table']['results']
-      elsif genre
-        @search_movies += JSON.parse((Tmdb::Genre.movies(params[:genre], page: "#{i}")).to_json)['table']['results']
+    if !params[:name].blank? && !params[:type].blank? #検索された場合
+      person_id = 0
+      if params[:type] == "1" #映画名が選択された場合
+        i = 1
+        JSON.parse((Tmdb::Search.movie(params[:name])).to_json)['table']['total_pages'].times do
+          @search_movies += JSON.parse((Tmdb::Search.movie(params[:name], page: "#{i}")).to_json)['table']['results']
+          i += 1
+          break if i > 6 # 120タイトル取得
+        end
+      elsif params[:type] == "2" #人物名が選択された場合
+        person_id = JSON.parse((Tmdb::Search.person(params[:name])).to_json)['table']['results'][0]['table']['id']
+        @search_movies = JSON.parse((Tmdb::Person.movie_credits(person_id)).to_json)['table']['cast']
       end
-      i += 1
-      break if i > 6
+      search = true
+    else #検索パラメーターがない場合
+      ('A'..'Z').to_a.shuffle[0..5].each do |random_chara| #ランダムで120タイトル取得
+        total_pages = JSON.parse((Tmdb::Search.movie(random_chara)).to_json)['table']['total_pages']
+        @search_movies += JSON.parse((Tmdb::Search.movie(random_chara, page: rand(1..total_pages.to_i))).to_json)['table']['results']
+      end
+      @search_movies.shuffle!
     end
 
-    #nameとgenreパラメータが両方ある場合
-    if name && !params[:genre].empty?
-      i = 0
-      kari = []
-      @search_movies.count.times do
+    #ジャンルが選択されていた場合
+    if !params[:genre].blank?
+      @search_movies.delete_if do |search_movie|
         #更にジャンルで絞る
-        if @search_movies[i]['table']['genre_ids'].include?(params[:genre].to_i)
-          kari.push @search_movies[i]
-        end
-        i += 1
+        !search_movie['table']['genre_ids'].include?(params[:genre].to_i)
       end
-      @search_movies = kari
     end
-    puts @search_movies
+
   end
-  
+
   def show
     @movie = JSON.parse((Tmdb::Movie.detail(params[:id])).to_json)['table']
     if @movie['poster_path'].blank?
